8a68a6e438bdf868308b50815acd5bcf0892d51b
Keiji O <meekunn@gmail.com>
2023-06-09 22:52:57 -0700

    code cleaned to remove design.js

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -1,5 +1,10 @@
 const Circle = require('../lib/circle.js');
 
 describe('Circle', () => {
-    
+    describe('render', () => {
+        it('should take in SVGshapeColor and produce shape portion for svg file', () => {
+            const color = 'blue'
+            const circle = new Circle()
+        })
+    })
 })
